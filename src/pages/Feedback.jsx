f648c53664eac64ba0b5ef035658b1ea235b4c87
coelhoreinaldo <scoelhor7@gmail.com>
2023-04-20 18:00:45 -0300

    feat: req 12

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1 data-testid="feedback-text">Feedbacks</h1>
       </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = ({ player }) => ({
+  email: player.gravatarEmail,
+  name: player.name,
+  score: player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
