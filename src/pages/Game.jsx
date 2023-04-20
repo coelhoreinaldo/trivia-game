5bd5134155cf2e3b21aa6487a75a14db3b09d355
andhuangg <anderson.huang93@gmail.com>
2023-04-20 16:14:21 -0300

    feat: req 10

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -93,8 +93,21 @@ class Game extends Component {
     return answer === correctAnswer ? 'green' : 'red';
   };
 
+  handleNextClick = () => {
+    this.setState(
+      (prevState) => ({
+        currentQuestionIndex: prevState.currentQuestionIndex + 1,
+        selectedAnswer: null,
+        timer: 30,
+      }),
+      () => this.randomAnswers(),
+    );
+  };
+
   render() {
-    const { questions, allAnswers, timer, currentQuestionIndex } = this.state;
+    const { questions, allAnswers, timer, currentQuestionIndex, selectedAnswer } = this.state;
+    const showNextButton = selectedAnswer !== null
+    && currentQuestionIndex < questions.length - 1;
 
     // Mensagem de carregamento enquanto as perguntas não são carregadas
     if (questions.length === 0) {
@@ -139,6 +152,14 @@ class Game extends Component {
               </button>
             </div>
           ))}
+          {showNextButton && (
+            <button
+              data-testid="btn-next"
+              onClick={ this.handleNextClick }
+            >
+              Next
+            </button>
+          )}
         </section>
         <section>
           <h1>{timer !== 0 ? timer : 'O tempo acabou'}</h1>
