287f59c5ee5d98cb5af3b5b9d5fd24ed105885b0
gabri10on <gabriel.r_00@hotmail.com>
2023-04-20 22:33:10 -0300

    feat: req 18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Ranking extends Component {
   render() {
+    const { history } = this.props;
     return (
       <div>
         <h1 data-testid="ranking-title">RANKING</h1>
+        <button
+          data-testid="btn-go-home"
+          onClick={ () => history.push('/') }
+        >
+          JOGAR NOVAMENTE
+        </button>
       </div>
     );
   }
 }
 
-// Ranking.propTypes = {
-
-// };
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default Ranking;
